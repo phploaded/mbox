faed37002a3ee01523ffce420b911c55aeda8d08
phploaded <satish@phploaded.com>
2013-11-15 01:41:13 +0530

    adding plugin files

---
+++ lib-mbox/ajax_load.php
@@ -0,0 +1,1 @@
+<?phperror_reporting(0);$homepage = file_get_contents($_GET['url']);$homepage = str_replace("script", "mboxdisablescript", $homepage);$homepage = str_replace("SCRIPT", "mboxdisablescript", $homepage);echo $homepage;?>
